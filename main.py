1a3a44ef41b21966922164e7ec8567506adb9dda
Igor <ziemniorcee@gmail.com>
2022-09-10 18:07:29 +0200

    se

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-from turtle import Screen, Turtle
+import turtle
+from turtle import Screen, Turtle, done
 
 # TODO Create a snake body
 
 screen = Screen()
 screen.bgcolor("black")
-
-
-
-body = []
 snake_length = 3
+body = []
+
 y = 0
 while y < 3:
     tur = Turtle(shape="square", visible=False)
@@ -19,12 +18,71 @@ while y < 3:
     body.append(tur)
     y += 1
 
-while True:
+
+def xd():
+    pass
+
+
+def travel():
+    n = 0
+    while n < 5:
+        x = 0
+        while x < snake_length:
+            body[x].forward(1)
+
+            x += 1
+        n += 1
+
+
+def go_up():
+    x = 0
+    start_x, start_y = body[0].pos()
+
+    while x < snake_length:
+        bod_x, bod_y = body[x].pos()
+        if bod_x >= start_x:
+            body[x].setheading(90)
+            x += 1
+
+        else:
+            while bod_x < start_x:
+                print(start_x, start_y)
+                print(bod_x, bod_y)
+                print(" ")
+                travel()
+                bod_x, bod_y = body[x].pos()
+    while True:
+        travel()
+
+
+def go_left():
     x = 0
     while x < snake_length:
-        body[x].forward(5)
+        body[x].setheading(180)
         x += 1
-    if body[0].xcor()+20 == screen.window_width() / 2:
-        break
+
+
+def go_right():
+    x = 0
+    while x < snake_length:
+        body[x].setheading(0)
+        x += 1
+
+
+def go_down():
+    x = 0
+    while x < snake_length:
+        body[x].setheading(270)
+        x += 1
+
+
+screen.onkey(go_up, 'Up')
+screen.onkey(go_left, 'Left')
+screen.onkey(go_right, 'Right')
+screen.onkey(go_down, 'Down')
+
+screen.listen()
+
+travel()
 
 screen.exitonclick()
