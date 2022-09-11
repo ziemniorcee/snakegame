ab011c7a0f66d46c079fb28fa0ba50648dff317f
Igor <ziemniorcee@gmail.com>
2022-09-11 11:54:11 +0200

    se

--- main.py
+++ main.py
@@ -1,116 +1,51 @@
-import turtle
-from turtle import Screen, Turtle, done
-
-# TODO Create a snake body
+from turtle import Screen, Turtle
 
 screen = Screen()
 screen.bgcolor("black")
-snake_length = 3
-body = []
 
-y = 0
-while y < 3:
-    tur = Turtle(shape="square", visible=False)
-    tur.color("white")
-    tur.penup()
-    tur.goto(-y * 15, 0)
-    tur.showturtle()
-    body.append(tur)
-    y += 1
+head = Turtle(shape="square")
+head.color("white")
+body_length = 2
 
 
-def xd():
-    pass
+def create_body():
+    snake_body = []
+    x = 0
+    while x < 2:
+        tur = Turtle(shape="square", visible=False)
+        tur.color("white")
+        tur.penup()
+        tur.goto(-x * 15, 0)
+        tur.speed('fastest')
+        tur.showturtle()
+        snake_body.append(tur)
+        x += 1
+    return snake_body
+
+
+def follow():
+    x = 0
+    while x < body_length:
+        if head.heading() != body[x].heading():
+
+
+def
 
 
 def travel():
-    n = 0
-    while n < 1:
-        x = 0
-        while x < snake_length:
-            body[x].forward(1)
-
-            x += 1
-        n += 1
+    head.forward(1)
+    screen.ontimer(travel, 10)
 
 
-def go_up():
-    x = 0
-    start_x, start_y = body[0].pos()
-    if turtle.heading() == 0:
-    while x < snake_length:
-        bod_x, bod_y = body[x].pos()
-        if bod_x >= start_x:
-            body[x].setheading(90)
-            x += 1
+screen.onkey(lambda: head.setheading(90), 'Up')
+screen.onkey(lambda: head.setheading(180), 'Left')
+screen.onkey(lambda: head.setheading(0), 'Right')
+screen.onkey(lambda: head.setheading(270), 'Down')
 
-        else:
-            while bod_x < start_x:
-                print(start_x, start_y)
-                print(bod_x, bod_y)
-                print(" ")
-                travel()
-                bod_x, bod_y = body[x].pos()
-    while True:
-        travel()
-
-
-def go_left():
-    x = 0
-    start_x, start_y = body[0].pos()
-
-    while x < snake_length:
-        bod_x, bod_y = body[x].pos()
-        if bod_y >= start_y:
-            body[x].setheading(180)
-            x += 1
-
-        else:
-            while bod_y < start_y:
-                print(start_x, start_y)
-                print(bod_x, bod_y)
-                print(" ")
-                travel()
-                bod_x, bod_y = body[x].pos()
-    while True:
-        travel()
-
-
-def go_right():
-    x = 0
-    while x < snake_length:
-        body[x].setheading(0)
-        x += 1
-
-
-def go_down():
-    x = 0
-    start_x, start_y = body[0].pos()
-
-    while x < snake_length:
-        bod_x, bod_y = body[x].pos()
-        if bod_x >= start_x:
-            body[x].setheading(270)
-            x += 1
-
-        else:
-            while bod_x < start_x:
-                print(start_x, start_y)
-                print(bod_x, bod_y)
-                print(" ")
-                travel()
-                bod_x, bod_y = body[x].pos()
-    while True:
-        travel()
-
-
-screen.onkey(go_up, 'Up')
-screen.onkey(go_left, 'Left')
-screen.onkey(go_right, 'Right')
-screen.onkey(go_down, 'Down')
+body = create_body()
 
 screen.listen()
 
 travel()
-
+follow()
 screen.exitonclick()
---
+++ new.py
@@ -0,0 +1,122 @@
+import turtle
+from turtle import Screen, Turtle, done
+
+# TODO Create a snake body
+
+screen = Screen()
+screen.bgcolor("black")
+snake_length = 3
+body = []
+
+y = 0
+while y < 3:
+    tur = Turtle(shape="square", visible=False)
+    tur.color("white")
+    tur.penup()
+    tur.goto(-y * 15, 0)
+    tur.speed('fastest')
+    tur.showturtle()
+    body.append(tur)
+    y += 1
+
+
+def travel():
+    n = 0
+    while n < 1:
+        x = 0
+        while x < snake_length:
+            body[x].forward(5)
+
+            x += 1
+        n += 1
+
+
+def go_up():
+
+    start_x, start_y = body[0].pos()
+    print("Change direction")
+    if body[0].heading() == 0:
+        x = 0
+        while x < snake_length:
+            bod_x, bod_y = body[x].pos()
+            if bod_x >= start_x:
+                body[x].setheading(90)
+                x += 1
+
+            else:
+                while bod_x < start_x:
+                    travel()
+                    bod_x, bod_y = body[x].pos()
+    elif body[0].heading() == 180:
+        x = 0
+        while x < snake_length:
+            bod_x, bod_y = body[x].pos()
+            if bod_x <= start_x:
+                body[x].setheading(90)
+                x += 1
+            else:
+                while not (bod_x < start_x + 5):
+                    travel()
+                    bod_x, bod_y = body[x].pos()
+
+    # while True:
+    #     travel()
+
+
+def go_left():
+    x = 0
+    start_x, start_y = body[0].pos()
+
+    while x < snake_length:
+        bod_x, bod_y = body[x].pos()
+        if bod_y >= start_y:
+            body[x].setheading(180)
+            x += 1
+
+        else:
+            while bod_y < start_y:
+
+                travel()
+                bod_x, bod_y = body[x].pos()
+    # while True:
+    #     travel()
+
+
+def go_right():
+    x = 0
+    while x < snake_length:
+        body[x].setheading(0)
+        x += 1
+
+
+def go_down():
+    x = 0
+    start_x, start_y = body[0].pos()
+
+    while x < snake_length:
+        bod_x, bod_y = body[x].pos()
+        if bod_x >= start_x:
+            body[x].setheading(270)
+            x += 1
+
+        else:
+            while bod_x < start_x:
+                print(start_x, start_y)
+                print(bod_x, bod_y)
+                print(" ")
+                travel()
+                bod_x, bod_y = body[x].pos()
+    while True:
+        travel()
+
+
+screen.onkey(go_up, 'Up')
+screen.onkey(go_left, 'Left')
+screen.onkey(go_right, 'Right')
+screen.onkey(go_down, 'Down')
+
+screen.listen()
+
+travel()
+
+screen.exitonclick()
