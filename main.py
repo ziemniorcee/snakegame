13e2ff2cbf59fcee23e06e6208c953f3e751eaad
Igor <ziemniorcee@gmail.com>
2022-09-12 11:42:05 +0200

    se

--- main.py
+++ main.py
@@ -1,36 +1,28 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 import time
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake game")
 screen.tracer(0)
-starting_position = [(0, 0), (-20, 0), (-40, 0)]
 
-segments = []
+snake = Snake()
 
-for position in starting_position:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+screen.listen()
+
+screen.onkey(snake.up, "Up")  # This will call the up function if the "Left" arrow key is pressed
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(1)
+    time.sleep(0.1)
 
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
-
-    segments[0].left(90)
-
-
-
+    snake.move()
 
 screen.exitonclick()
